f40c8cdf76e0d7895eb19dc2762766581e1073b8
Marco Tomba <155143723+CamatoDev@users.noreply.github.com>
2026-01-13 10:20:17 +0100

    Add initial Eureka Server Spring Boot project
    Set up a new Spring Boot project for EurekaServerWillBank with Eureka Server enabled. Includes Maven configuration, application properties, main application class, and a basic test class.

---
+++ EurekaServerWillBank/src/main/java/com/vortexmakers/EurekaServerWillBank/EurekaServerWillBankApplication.java
@@ -0,0 +1,13 @@
+package com.vortexmakers.EurekaServerWillBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerWillBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerWillBankApplication.class, args);
+    }
+}
---
+++ EurekaServerWillBank/src/test/java/com/vortexmakers/EurekaServerWillBank/EurekaServerWillBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexmakers.EurekaServerWillBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerWillBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
