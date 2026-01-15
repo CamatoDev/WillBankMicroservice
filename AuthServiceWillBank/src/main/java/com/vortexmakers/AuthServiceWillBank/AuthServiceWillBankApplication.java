68ed4ea3b4083a7986214c2e6a22e439802689ba
Marco Tomba <155143723+CamatoDev@users.noreply.github.com>
2026-01-15 21:22:55 +0100

    Initial commit for AuthServiceWillBank service
    Add Spring Boot project setup for AuthServiceWillBank microservice, including Maven configuration, application entry point, basic test, and project metadata files.

---
+++ AuthServiceWillBank/src/main/java/com/vortexmakers/AuthServiceWillBank/AuthServiceWillBankApplication.java
@@ -0,0 +1,13 @@
+package com.vortexmakers.AuthServiceWillBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceWillBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceWillBankApplication.class, args);
+	}
+
+}
