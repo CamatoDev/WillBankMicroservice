9fa7384c0da931557d6d30059de48356da36e3be
Marco Tomba <155143723+CamatoDev@users.noreply.github.com>
2026-01-15 19:10:13 +0100

    Initial commit for CompositeServiceWillBank service
    Add base Spring Boot microservice project with Eureka client and OpenFeign dependencies. Includes Maven wrapper, project structure, application entry point, and a basic test to verify context loading.

---
+++ CompositeServiceWillBank/src/main/java/com/vortexmakers/CompositeServiceWillBank/CompositeServiceWillBankApplication.java
@@ -0,0 +1,13 @@
+package com.vortexmakers.CompositeServiceWillBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeServiceWillBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeServiceWillBankApplication.class, args);
+	}
+
+}
---
+++ CompositeServiceWillBank/src/test/java/com/vortexmakers/CompositeServiceWillBank/CompositeServiceWillBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexmakers.CompositeServiceWillBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeServiceWillBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
